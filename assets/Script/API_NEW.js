96c904ad0db58efb6439f40537ffe5da48eebc99
Shorino <simkalok0914@gmail.com>
2021-08-18 12:26:54 +0800

    Added prod socket url #3

--- assets/Script/API_NEW.js
+++ assets/Script/API_NEW.js
@@ -7,6 +7,22 @@ cc.Class({
 
     onLoad: function()
     {
-        
+        cc.log("--------- Dynamic Endpoint ----------------");
+        if(window.endPointConfig != null){
+            var networkConfig = ecryptContoller.decrypt(window.endPointConfig);
+            if(networkConfig != null){
+                var networkConfigJson = JSON.parse(networkConfig);
+
+                //cc.log(networkConfigJson.geoip_url);
+                //cc.log(networkConfigJson.api_url);
+
+                global.geoIp_url = networkConfigJson.geoip_url;
+                global.api_url = networkConfigJson.api_url;
+                constant.socketURL = constant.prodSocketURL;
+
+                // cc.log(global.SetGeoip_Url(networkConfigJson.geoip_url));
+                // cc.log(global.SetApi_Url(networkConfigJson.api_url));
+            }
+        }
     },
 });
